f8f4e8c932bbe9f326849915adf40863b7f6b542
romaingallez <74435156+romaingallez@users.noreply.github.com>
2023-04-12 20:41:36 +0200

    Change config type from yaml to json. Fix typo in readme

--- cmd/root.go
+++ cmd/root.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,6 +18,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+
 	"github.com/spf13/cobra"
 
 	"github.com/spf13/viper"
@@ -72,7 +73,7 @@ func initConfig() {
 
 		// Search config in home directory with name ".proxmox-spice-cli" (without extension).
 		viper.AddConfigPath(home)
-		viper.SetConfigType("yaml")
+		viper.SetConfigType("json")
 		viper.SetConfigName(".proxmox-spice-cli")
 	}
 
